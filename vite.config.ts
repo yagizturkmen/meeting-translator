9f9a993603fed09f67f2d62dd82e564684086bce
Yagiz <yagiz@peilabs.com>
2025-05-21 02:15:09 +0300

    improve DX and add TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   base: './',
   build: {
     outDir: 'dist-react'
+  },
+  server: {
+    port: 5123,
+    strictPort: true,
   }
 })
