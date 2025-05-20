9699b2033dcbbeaf5a1826b96e96570f34f3fa51
Yagiz <yagiz@peilabs.com>
2025-05-20 20:04:43 +0300

    added Typescript and Electron-Builder

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 
+type test = string;
+
 app.on('ready', () => {
   const maainWindow = new BrowserWindow({});
   maainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
